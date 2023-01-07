01ba4442f510afd4fef4314e7daf7534105969e5
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-01-08 01:35:55 +0600

    Add files via upload

---
+++ header/connection.php
@@ -0,0 +1,4 @@
+<?php
+$db=new PDO('mysql:host=localhost:3307;dbname=bdm','root','');
+$con =mysqli_connect("localhost:3307","root","","bdm");
+ ?>
---
+++ header/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <title>Blood Donation | Welcome</title>
+    <link rel="stylesheet" href="./css/style.css">
+  
+  </head>
+
+  <body>
+<header>
+      <h1><img src="img/cover2.svg" alt="" WIDTH=100 HEIGHT=80/>   Blood Donation Managment</h1>
+
+</header>
+
+
+</body>
+</html>
